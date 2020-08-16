7bb2bc8a6cb5e27b84ebf1e573be4e692ea755b7
danibachar <danibachar89@gmail.com>
2020-08-16 15:10:47 +0300

    Daniel test (#3)
    * testing
    
    * wip
    
    * finished!!!
    
    * fixes + accurecy

--- functions/backup.py
+++ functions/backup.py
@@ -5,20 +5,23 @@ import pandas as pd
 from .utils import build_remote_and_local_file_names
 from .core.storage import upload_file
 
-def save_locally_and_update(df, original_dfs, cleaning_dfs):
-  urls = []
-  for key, val in original_dfs.items():
-      remote, local = build_remote_and_local_file_names("original","csv")
-      val.to_csv(local, index=False)
-      urls += [upload_file(local, remote),]
+def save_locally_and_update(df, original_dfs=None, cleaning_dfs=None):
+    original_files_urls = []
+    if original_dfs:
+        for key, val in original_dfs.items():
+          remote, local = build_remote_and_local_file_names(key,"csv")
+          val.to_csv(local, index=False)
+          original_files_urls += [upload_file(local, remote),]
 
-  for key, val in cleaning_dfs.items():
-      remote, local = build_remote_and_local_file_names("clean","csv")
-      val.to_csv(local, index=False)
-      urls += [upload_file(local, remote),]
+    cleaning_files_urls = []
+    if cleaning_dfs:
+        for key, val in cleaning_dfs.items():
+          remote, local = build_remote_and_local_file_names(key,"csv")
+          val.to_csv(local, index=False)
+          clean_files_urls += [upload_file(local, remote),]
 
-  remote, local = build_remote_and_local_file_names("primary","csv")
-  df.to_csv(local, index=False)
-  urls += [upload_file(local, remote),]
+    remote, local = build_remote_and_local_file_names("primary","csv")
+    df.to_csv(local, index=False)
+    primary_file_urls = [upload_file(local, remote),]
 
-  return urls
+    return original_files_urls,cleaning_files_urls,primary_file_urls
--- functions/summary.py
+++ functions/summary.py
@@ -4,6 +4,90 @@ import pandas as pd
 from .utils import build_remote_and_local_file_names
 from .core.storage import upload_file
 
+def accurecy_calculator(before_df, after_df, group_by, unique_key, features_col_names):
+    before = before_df.copy()
+    after = after_df.copy()
+    res_before, res_after = {}, {}
+    dfs = []
+    def fill_res(groups, results):
+        for group in groups:
+          g = group[-1]
+          group_col = g[group_by]
+          group_id = group_by+"="+str(group_col.iloc[0])
+          results[group_id] = {}
+          parsed_feature_set = set()
+          for first_feature_name in features_col_names:
+              for second_feature_name in features_col_names:
+                  if first_feature_name == second_feature_name:
+                      continue
+                  first_feature_values = g[first_feature_name].unique()
+                  second_feature_values = g[second_feature_name].unique()
+                  for f_val in first_feature_values:
+                    for s_val in second_feature_values:
+                        fkey1 = "{} X {}".format(f_val, s_val)
+                        fkey2 = "{} X {}".format(s_val, f_val)
+                        if fkey1 in parsed_feature_set or fkey2 in parsed_feature_set:
+                            continue
+                        parsed_feature_set.add(fkey1)
+                        parsed_feature_set.add(fkey2)
+                        sel = (g[first_feature_name] == f_val) & (g[second_feature_name] == s_val)
+                        f = first_feature_name+"="+str(f_val)
+                        s = second_feature_name+"="+str(s_val)
+                        name = f+" / "+s
+
+                        results[group_id][name] = g[sel][unique_key].nunique()
+    groups_before = before.groupby(by=[group_by])
+    groups_after = after.groupby(by=[group_by])
+    fill_res(groups_before, res_before)
+    fill_res(groups_after, res_after)
+
+
+    for key, b in res_before.items():
+        rrrr = {}
+        a = res_after.get(key,{})
+        count_before = []
+        count_after = []
+        accurecy = []
+
+        for k,v in b.items():
+            if v==0:
+                continue
+            grrrr = k.split("/")
+            c1 = grrrr[0].split("=")
+            name1 = c1[0]
+            val1 = c1[1]
+            c2 = grrrr[1].split("=")
+            name2 = c2[0]
+            val2 = c2[1]
+            if name1 in rrrr:
+                rrrr[name1].append(val1)
+            else:
+                rrrr[name1] = [val1]
+
+            if name2 in rrrr:
+                rrrr[name2].append(val2)
+            else:
+                rrrr[name2] = [val2]
+            af = a.get(k,0)
+            count_before.append(v)
+            count_after.append(af)
+            acc = af/v
+            accurecy.append(acc)
+
+        rrrr["participant"] = [key]*len(count_before)
+        rrrr["count_before"] = count_before
+        rrrr["count_after"] = count_after
+        rrrr["accurecy"] = accurecy
+        try:
+            dfs.append(pd.DataFrame(rrrr))
+        except Exception as e:
+            pass
+
+    remote, local = build_remote_and_local_file_names("accurecy","csv")
+    pd.concat(dfs).to_csv(local)
+    return upload_file(local, remote)
+
+
 def info_dfs(dfs):
     remote, local = build_remote_and_local_file_names("info","txt")
     f =  open(local, 'w')
@@ -11,6 +95,9 @@ def info_dfs(dfs):
     sys.stdout = f
     for df in dfs:
         print(df.info())
+        print("Null/Nan Values\nPlease note, Null/Nan values could indicate a problem with your data")
+        print("On the left you will see the relevant column name, and on the right the amount of Null/Nan rows for that column")
+        print(df.isnull().sum())
         for col in df.columns:
             u = df[col].unique()
             print("{} has {} unique values".format(col, len(u)))
